fe182b46a68053541138519369cafa8b34efac75
jayant1992 <jayantjain1992@gmail.com>
2014-02-08 17:30:06 +0530

    Removes useless classes and statements, adds size, vel and pos params for creating balls

--- KinectGame2/Gulp.py
+++ KinectGame2/Gulp.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python
-#
-# Tom's Pong
-# A simple pong game with realistic physics and AI
-# http://www.tomchance.uklinux.net/projects/pong.shtml
-#
-# Released under the GNU General Public License
-VERSION = "0.4"
 
 try:
   import sys
@@ -14,11 +7,15 @@ try:
   import os
   import getopt
   import pygame
+  from time import sleep
   from socket import *
   from pygame.locals import *
 except ImportError, err:
   print "couldn't load module. %s" % (err)
   sys.exit(2)
+
+ASPECT_RATIO = (640, 480)
+SCALE = 10
 
 def load_png(name):
   """ Load image and return image object"""
@@ -32,7 +29,7 @@ def load_png(name):
   except pygame.error, message:
     print 'Cannot load image:', fullname
     raise SystemExit, message
-  return image, image.get_rect()
+  return image
 
 class RandomBall(pygame.sprite.Sprite):
   """A ball that keeps on moving in a particular direction
@@ -165,120 +162,51 @@ class RandomObjectGroup(pygame.sprite.Group):
     pygame.sprite.Group.__init__(self);
 
 class Ball(pygame.sprite.Sprite):
-  """A ball that will move across the screen
+  """Base class for general world objects
   Returns: ball object
   Functions: update, calcnewpos
   Attributes: area, vector"""
 
-  def __init__(self, (xy), vector):
+  def __init__(self, (pos), vel, rad):
     pygame.sprite.Sprite.__init__(self)
-    self.image, self.rect = load_png('ball.png')
+
     screen = pygame.display.get_surface()
-    self.area = screen.get_rect()
-    self.vector = vector
-    self.speed = 10
-    self.hit = 0
+
+    self.image = load_png('ball.png')
+    self.image = pygame.transform.scale(self.image, (rad*SCALE, rad*SCALE))
+
+    self.rect = self.image.get_rect()
+    self.rect.x = pos[0]
+    self.rect.y = pos[1]
+
+    self.speed = 1
+    self.vel = vel
     self.state = "still"
-    self.movepos = [0,0]
 
   def update(self):
-    #newpos = self.calcnewpos(self.rect,self.vector)
-    newpos = self.rect.move(self.movepos)
+    newpos = self.rect.move(self.vel)
     self.rect = newpos
-    (angle,z) = self.vector
 
-    if not self.area.contains(newpos):
-      tl = not self.area.collidepoint(newpos.topleft)
-      tr = not self.area.collidepoint(newpos.topright)
-      bl = not self.area.collidepoint(newpos.bottomleft)
-      br = not self.area.collidepoint(newpos.bottomright)
-      if tr and tl or (br and bl):
-        angle = -angle
-      if tl and bl:
-        #self.offcourt()
-        angle = math.pi - angle
-      if tr and br:
-        angle = math.pi - angle
-        #self.offcourt()
-    else:
-      # Deflate the rectangles so you can't catch a ball behind the bat
-      player1.rect.inflate(-3, -3)
-      player2.rect.inflate(-3, -3)
-
-      # Do ball and bat collide?
-      # Note I put in an odd rule that sets self.hit to 1 when they collide, and unsets it in the next
-      # iteration. this is to stop odd ball behaviour where it finds a collision *inside* the
-      # bat, the ball reverses, and is still inside the bat, so bounces around inside.
-      # This way, the ball can always escape and bounce away cleanly
-      if self.rect.colliderect(player1.rect) == 1 and not self.hit:
-        angle = math.pi - angle
-        self.hit = not self.hit
-      elif self.rect.colliderect(player2.rect) == 1 and not self.hit:
-        angle = math.pi - angle
-        self.hit = not self.hit
-      elif self.hit:
-        self.hit = not self.hit
-    self.vector = (angle,z)
-
-  def calcnewpos(self, rect, vector):
-    (angle,z) = vector
-    (dx,dy) = (z * math.cos(angle), z * math.sin(angle))
-    return rect.move(dx,dy)
-  
   def moveup(self):
-    self.movepos[1] = self.movepos[1] - (self.speed)
+    self.vel[1] = self.vel[1] - (self.speed)
     self.state = "moveup"
 
   def moveleft(self):
-    self.movepos[0] = self.movepos[0] - (self.speed)
+    self.vel[0] = self.vel[0] - (self.speed)
     self.state = "moveleft"
 
   def moveright(self):
-    self.movepos[0] = self.movepos[0] + (self.speed)
+    self.vel[0] = self.vel[0] + (self.speed)
     self.state = "moveright"
 
   def movedown(self):
-    self.movepos[1] = self.movepos[1] + (self.speed)
+    self.vel[1] = self.vel[1] + (self.speed)
     self.state = "movedown"
-
-class Bat(pygame.sprite.Sprite):
-  def __init__(self, side):
-    pygame.sprite.Sprite.__init__(self)
-    self.image, self.rect = load_png('bat.png')
-    screen = pygame.display.get_surface()
-    self.area = screen.get_rect()
-    self.side = side
-    self.speed = 10
-    self.state = "still"
-    self.reinit()
-
-  def reinit(self):
-    self.state = "still"
-    self.movepos = [0,0]
-    if self.side == "left":
-      self.rect.midleft = self.area.midleft
-    elif self.side == "right":
-      self.rect.midright = self.area.midright
-
-  def update(self):
-    newpos = self.rect.move(self.movepos)
-    if self.area.contains(newpos):
-      self.rect = newpos
-    pygame.event.pump()
-
-  def moveup(self):
-    self.movepos[1] = self.movepos[1] - (self.speed)
-    self.state = "moveup"
-
-  def movedown(self):
-    self.movepos[1] = self.movepos[1] + (self.speed)
-    self.state = "movedown"
-
 
 def main():
   # Initialise screen
   pygame.init()
-  screen = pygame.display.set_mode((640, 480))
+  screen = pygame.display.set_mode(ASPECT_RATIO)
   pygame.display.set_caption('The Masculine Ping Pong Game')
 
   # Fill background
@@ -286,28 +214,15 @@ def main():
   background = background.convert()
   background.fill((0, 0, 0))
 
-  # Initialise players
-  global player1
-  global player2
-  player1 = Bat("left")
-  player2 = Bat("right")
-
   # Initialise ball
   speed = 4
   rand = ((0.1 * (random.randint(5,8))))
-  ball = Ball((0,0),(0.47, speed))
-  
-  ball2 = RandomBall((0,0),(0.47, speed))
-  ball3 = RandomBall((200,0),(-1, speed), 40)
 
-  global randomballs
-  randomballs = []
-  randomballs.append(ball2)
-  randomballs.append(ball3)
+  ball = Ball((50,0),[0, 0], 5)
+  ball2 = Ball((0,50),[0, 0], 15)
 
   # Initialise sprites
-  playersprites = pygame.sprite.RenderPlain((player1, player2))
-  ballsprites = pygame.sprite.RenderPlain(ball, ball2, ball3)
+  ballsprites = pygame.sprite.RenderPlain(ball, ball2)
 
   # Blit everything to the screen
   screen.blit(background, (0, 0))
@@ -347,11 +262,13 @@ def main():
           #player2.movedown()
       elif event.type == KEYUP:
         if event.key == K_w or event.key == K_s or event.key == K_a or event.key == K_d:
-          ball.movepos = [0, 0]
-          ball.state = "still"
+          pass
+          # ball.vel = [0, 0]
+          # ball.state = "still"
         if event.key == K_UP or event.key == K_DOWN or event.key == K_LEFT or event.key == K_RIGHT:
-          ball2.movepos = [0, 0]
-          ball2.state = "still"
+          pass
+          # ball2.vel = [0, 0]
+          # ball2.state = "still"
         """if event.key == K_w or event.key == K_s:
           ball.state = "still"
           #player1.movepos = [0,0]
@@ -363,13 +280,11 @@ def main():
 
     screen.blit(background, ball.rect, ball.rect)
     screen.blit(background, ball2.rect, ball2.rect)
-    screen.blit(background, ball3.rect, ball3.rect)
-    screen.blit(background, player1.rect, player1.rect)
-    screen.blit(background, player2.rect, player2.rect)
+
     ballsprites.update()
-    playersprites.update()
+    # playersprites.update()
     ballsprites.draw(screen)
-    playersprites.draw(screen)
+    # playersprites.draw(screen)
     pygame.display.flip()
 
 if __name__ == '__main__':
